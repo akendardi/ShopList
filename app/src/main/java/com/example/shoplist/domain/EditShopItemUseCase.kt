64b9d795387846ba54a276a845fd387018536efb
Aleksandr <akendardi@mail.ru>
2024-02-27 17:18:22 +0300

    Create UseCases

---
+++ app/src/main/java/com/example/shoplist/domain/EditShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoplist.domain
+
+class EditShopItemUseCase {
+    fun editShopItem(id: Int){
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/shoplist/domain/GetShopListUserCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoplist.domain
+
+class GetShopListUserCase {
+    fun getShopingList() : List<ShopItem> {
+        TODO()
+    }
+}
