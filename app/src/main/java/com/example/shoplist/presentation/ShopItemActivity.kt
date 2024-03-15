7941d08fe599b0d7e6fbb2e6dde4074424fb0b90
Aleksandr <akendardi@mail.ru>
2024-03-15 19:48:14 +0300

    clean code

--- app/src/main/java/com/example/shoplist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoplist/presentation/ShopItemActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
@@ -35,6 +34,45 @@ class ShopItemActivity : AppCompatActivity() {
         initViews()
         shopItemViewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
 
+
+        when (extraMode) {
+            EXTRA_MODE_ADD_ITEM -> launchAddMode()
+            EXTRA_MODE_EDIT_ITEM -> launchEditMode()
+        }
+        textWatchers()
+        observers()
+    }
+
+    companion object {
+        private const val EXTRA_MODE = "extra_mode"
+        private const val EXTRA_MODE_ADD_ITEM = "extra_mode_add_item"
+        private const val EXTRA_MODE_EDIT_ITEM = "extra_mode_edit_item"
+        private const val EXTRA_ID = "id"
+        private const val EMPTY_STRING = ""
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_MODE, EXTRA_MODE_ADD_ITEM)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, id: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_MODE, EXTRA_MODE_EDIT_ITEM)
+            intent.putExtra(EXTRA_ID, id)
+            return intent
+        }
+    }
+
+    private fun initViews() {
+        til_name = findViewById(R.id.til_name)
+        til_count = findViewById(R.id.til_count)
+        et_name = findViewById(R.id.et_name)
+        et_count = findViewById(R.id.et_count)
+        button_save = findViewById(R.id.buttonSave)
+    }
+
+    private fun textWatchers() {
         et_name.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
@@ -62,16 +100,14 @@ class ShopItemActivity : AppCompatActivity() {
 
             }
         })
-        when (extraMode){
-            EXTRA_MODE_ADD_ITEM -> launchAddMode()
-            EXTRA_MODE_EDIT_ITEM -> launchEditMode()
-        }
+    }
 
-        shopItemViewModel.canCloseScreen.observe(this){
+    private fun observers() {
+        shopItemViewModel.canCloseScreen.observe(this) {
             finish()
         }
 
-        shopItemViewModel.errorInputName.observe(this){
+        shopItemViewModel.errorInputName.observe(this) {
             val message = if (it) {
                 getString(R.string.input_correct)
             } else {
@@ -80,7 +116,7 @@ class ShopItemActivity : AppCompatActivity() {
             til_name.error = message
         }
 
-        shopItemViewModel.errorInputCount.observe(this){
+        shopItemViewModel.errorInputCount.observe(this) {
             val message = if (it) {
                 getString(R.string.input_correct)
             } else {
@@ -88,51 +124,21 @@ class ShopItemActivity : AppCompatActivity() {
             }
             til_count.error = message
         }
-
     }
 
-    companion object {
-        private const val EXTRA_MODE = "extra_mode"
-        private const val EXTRA_MODE_ADD_ITEM = "extra_mode_add_item"
-        private const val EXTRA_MODE_EDIT_ITEM = "extra_mode_edit_item"
-        private const val EXTRA_ID = "id"
-        private const val EMPTY_STRING = ""
-
-        fun newIntentAddItem(context: Context): Intent {
-            val intent = Intent(context, ShopItemActivity::class.java)
-            intent.putExtra(EXTRA_MODE, EXTRA_MODE_ADD_ITEM)
-            return intent
-        }
-
-        fun newIntentEditItem(context: Context, id: Int): Intent {
-            val intent = Intent(context, ShopItemActivity::class.java)
-            intent.putExtra(EXTRA_MODE, EXTRA_MODE_EDIT_ITEM)
-            intent.putExtra(EXTRA_ID, id)
-            return intent
-        }
-    }
-
-    private fun initViews(){
-        til_name = findViewById(R.id.til_name)
-        til_count = findViewById(R.id.til_count)
-        et_name = findViewById(R.id.et_name)
-        et_count = findViewById(R.id.et_count)
-        button_save = findViewById(R.id.buttonSave)
-    }
-
-    private fun parseIntent(){
+    private fun parseIntent() {
         val intent = intent
         val intentExtraMode = intent.getStringExtra(EXTRA_MODE)
-        if (!intent.hasExtra(EXTRA_MODE)){
+        if (!intent.hasExtra(EXTRA_MODE)) {
             throw RuntimeException("Param mode empty")
         }
-        if (intentExtraMode != EXTRA_MODE_ADD_ITEM && intentExtraMode != EXTRA_MODE_EDIT_ITEM){
+        if (intentExtraMode != EXTRA_MODE_ADD_ITEM && intentExtraMode != EXTRA_MODE_EDIT_ITEM) {
             throw RuntimeException("Param is unknown")
         }
-        if (intentExtraMode == EXTRA_MODE_ADD_ITEM){
+        if (intentExtraMode == EXTRA_MODE_ADD_ITEM) {
             extraMode = EXTRA_MODE_ADD_ITEM
         } else {
-            if (!intent.hasExtra(EXTRA_ID)){
+            if (!intent.hasExtra(EXTRA_ID)) {
                 throw RuntimeException("Not id")
             } else {
                 extraMode = EXTRA_MODE_EDIT_ITEM
@@ -142,19 +148,19 @@ class ShopItemActivity : AppCompatActivity() {
     }
 
 
-    private fun launchEditMode(){
+    private fun launchEditMode() {
         shopItemViewModel.getShopItem(id)
-        shopItemViewModel.shopItem.observe(this){
+        shopItemViewModel.shopItem.observe(this) {
             et_name.setText(it.name)
             et_count.setText(it.count.toString())
-            button_save.setOnClickListener{
+            button_save.setOnClickListener {
                 shopItemViewModel.editShopItem(et_name.text?.toString(), et_count.text?.toString())
             }
         }
     }
 
-    private fun launchAddMode(){
-        button_save.setOnClickListener{
+    private fun launchAddMode() {
+        button_save.setOnClickListener {
             shopItemViewModel.addShopItem(et_name.text?.toString(), et_count.text?.toString())
         }
     }
